bd3d4bc94ed72d8d918a1b4a7da05b8965b44519
davidblancolopez <daviddbl94@gmail.com>
2016-12-09 17:53:55 +0100

    Cambios.

--- app/src/main/java/com/example/alumnedam/activitat_3_android/MainActivity.java
+++ app/src/main/java/com/example/alumnedam/activitat_3_android/MainActivity.java
@@ -18,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
     String horari = "CREATE TABLE horari (id_horari INTEGER, grup TEXT, codi_assignatura TEXT, hora_inici TEXT, hora_fi TEXT, dia_setmana INTEGER)";
 
-    String assignatura = "CREATE TABLE assignatura (id_assignatura TEXT, professor TEXT)";
+    String assignatura = "CREATE TABLE assignatura (id_assignatura TEXT, assignatura TEXT, id_professor INTEGER)";
 
-    
+    String professors = "CREATE TABLE professors (id_professor TEXT, nom_professor TEXT)";
+
+    String [] sentenciaINSERTassignatura = {"INSERT INTO assignatura (1, M2-M5-M6, )"};
+
+    String [] sentenciasINSERTprofessors = {"INSERT INTO professors (1, Jorge Rubio)", "INSERT INTO professors (2, Josefa Gonzalez)" ,
+            "INSERT INTO professors (3, Jose Antonio Leo)", "INSERT INTO professors (4, Lluis Maria Perpinya)" ,"INSERT INTO professors (5, Marta Planas)"};
 }
