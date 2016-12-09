deb26de96022b08b676c8c0027b0d37f482c88de
davidblancolopez <daviddbl94@gmail.com>
2016-12-09 17:25:49 +0100

    Cambios.

--- app/src/main/java/com/example/alumnedam/activitat_3_android/MainActivity.java
+++ app/src/main/java/com/example/alumnedam/activitat_3_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.alumnedam.activitat_3_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    String horari = "CREATE TABLE horari (id_horari INTEGER, grup TEXT, codi_assignatura TEXT, hora_inici TEXT, hora_fi TEXT, dia_setmana INTEGER)";
+
+    String assignatura = "CREATE TABLE assignatura (id_assignatura TEXT, professor TEXT)";
+
+    
 }
